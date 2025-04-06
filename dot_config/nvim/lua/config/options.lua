a392403d8c38065bebc759973f012c927f76be6d
jecepeda <jcepeda@hey.com>
2025-04-06 23:58:46 +0200

    diff between sshing and not

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,6 +2,13 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 --
+
+-- if running linux it means I'm working remotely so we should use
+--
+if vim.loop.os_uname().sysname == "Linux" then
+  vim.opt.clipboard = "osc52"
+else
+  vim.opt.clipboard = "unnamedplus"
+end
 vim.g.mapleader = ","
 vim.g.maplocalleader = "\\"
-vim.opt.clipboard = "unnamedplus"
